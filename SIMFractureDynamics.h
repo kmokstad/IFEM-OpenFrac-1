ae91d704aba2293c8051d1c89e7942b5bd0f7b70
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2017-11-15 09:47:08 +0100

    some fixes to make subiterations work
    not clean, i have tried to put comments as to why the changes have been done

--- SIMFractureDynamics.h
+++ SIMFractureDynamics.h
@@ -240,7 +240,8 @@ public:
     }
 
     size_t solsize = sols.size();
-    size_t sollen = sols.front().size();
+    // sanity for some case. cannot recall
+    size_t sollen = sols.empty() ? 0 : sols.front().size();
     if (!this->S1.refine(prm,sols) || !this->S2.refine(prm))
       return -2;
 
