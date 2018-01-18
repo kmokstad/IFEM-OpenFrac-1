OPM/IFEM-OpenFrac

Added: class SIMExplPhaseField representing an explicit phase field, and new command-line options -explcrack to activate it

main_FractureDynamics.C
@@ -16,6 +16,7 @@
 #include "SIM3D.h"
 #include "SIMDynElasticity.h"
 #include "SIMPhaseField.h"
+#include "SIMExplPhaseField.h"
 #include "SIMFractureDynamics.h"
 #ifdef IFEM_HAS_POROELASTIC
 #include "SIMPoroElasticity.h"
@@ -42,11 +43,12 @@ struct FDargs
   char coupling;  //!< Coupling flag (0: none, 1: staggered, 2: semi-implicit)
   bool poroEl;    //!< If \e true, use the poroelastic solver
   bool adaptive;  //!< If \e true, use the time-slab adaptive solver
+  bool explPhase; //!< If \e true, use an explicit phase field
   char* inpfile;  //!< The input file to parse
 
   //! \brief Default constructor.
   FDargs() : inpfile(nullptr)
-  { coupling = integrator = 1; poroEl = adaptive = false; }
+  { coupling = integrator = 1; poroEl = adaptive = explPhase = false; }
 };
 
 
@@ -163,7 +165,7 @@ int runCombined (char* infile, const char* context)
 template<class ElSolver, class PhaseSolver,
          template<class T1, class T2> class Cpl,
          template<class T1> class Solver=SIMSolver>
-int runSimulator5 (const FDargs& args, const char* contx)
+int runSimulator6 (const FDargs& args, const char* contx)
 {
   if (args.integrator == 3 && args.coupling == 2)
   {
@@ -175,6 +177,22 @@ int runSimulator5 (const FDargs& args, const char* contx)
     typedef SIMFracture<ElSolver,PhaseSolver,Cpl> Coupler;
     return runCombined<ElSolver,PhaseSolver,Coupler,Solver>(args.inpfile,contx);
   }
+}
+
+
+/*!
+  \brief Determines whether the explicit phase-field driver is to be used.
+*/
+
+template<class Dim, class ElSolver,
+         template<class T1, class T2> class Cpl,
+         template<class T1> class Solver=SIMSolver>
+int runSimulator5 (const FDargs& args, const char* context)
+{
+  if (args.explPhase)
+    return runSimulator6<ElSolver,SIMExplPhaseField,Cpl,Solver>(args,context);
+  else
+    return runSimulator6<ElSolver,SIMPhaseField<Dim>,Cpl,Solver>(args,context);
 }
 
 
@@ -259,14 +277,13 @@ template<class Dim, class Integrator, class ElSolver,
 int runSimulator3 (const FDargs& args)
 {
   typedef SIMDynElasticity<Dim,Integrator,ElSolver> DynElSolver;
-  typedef SIMPhaseField<Dim>                        PhaseSolver;
 
   const char* context = Integrator::inputContext;
 
   if (args.adaptive)
-    return runSimulator5<DynElSolver,PhaseSolver,Cpl,SIMSolverTS>(args,context);
+    return runSimulator5<Dim,DynElSolver,Cpl,SIMSolverTS>(args,context);
 
-  return runSimulator5<DynElSolver,PhaseSolver,Cpl>(args,context);
+  return runSimulator5<Dim,DynElSolver,Cpl>(args,context);
 }
 
 
@@ -369,6 +386,8 @@ int main (int argc, char** argv)
       twoD = SIMElasticity<SIM2D>::planeStrain = true;
     else if (!strcmp(argv[i],"-mixed"))
       ASMmxBase::Type = ASMmxBase::FULL_CONT_RAISE_BASIS1;
+    else if (!strcmp(argv[i],"-explcrack"))
+      args.explPhase = true;
     else if (!strcmp(argv[i],"-nocrack"))
       args.coupling = 0;
     else if (!strcmp(argv[i],"-semiimplicit"))
@@ -399,10 +418,11 @@ int main (int argc, char** argv)
   if (!args.inpfile)
   {
     std::cout <<"usage: "<< argv[0]
-              <<" <inputfile> [-dense|-spr|-superlu[<nt>]|-samg|-petsc]\n      "
-              <<" [-lag|-spec|-LR] [-2D] [-mixed] [-nGauss <n>]\n       [-nocra"
-              <<"ck|-semiimplicit] [-[l|q]static|-GA|-HHT] [-poro] [-adaptive]"
-              <<"\n       [-vtf <format> [-nviz <nviz>] [-nu <nu>] [-nv <nv]"
+              <<" <inputfile> [-dense|-spr|-superlu[<nt>]|-samg|-petsc]\n"
+              <<"       [-lag|-spec|-LR] [-2D] [-mixed] [-nGauss <n>]\n"
+              <<"       [-nocrack|-explcrack|-semiimplicit]"
+              <<" [-[l|q]static|-GA|-HHT] [-poro] [-adaptive]\n"
+              <<"       [-vtf <format> [-nviz <nviz>] [-nu <nu>] [-nv <nv]"
               <<" [-nw <nw>]]\n       [-hdf5] [-principal]\n"<< std::endl;
     return 0;
   }

kmokstad/IFEM-OpenFrac-1

main_FractureDynamics.C
@@ -16,6 +16,7 @@
 #include "SIM3D.h"
 #include "SIMDynElasticity.h"
 #include "SIMPhaseField.h"
+#include "SIMExplPhaseField.h"
 #include "SIMFractureQstatic.h"
 #ifdef IFEM_HAS_POROELASTIC
 #include "SIMPoroElasticity.h"
@@ -143,7 +144,7 @@ int runCombined (char* infile, const char* context)
 template<class ElSolver, class PhaseSolver,
          template<class T1, class T2> class Cpl,
          template<class T1> class Solver=SIMSolver>
-int runSimulator5 (const FractureArgs& args, const char* contx)
+int runSimulator6 (const FractureArgs& args, const char* contx)
 {
   if (args.integrator == 3 && args.coupling == 2)
   {
@@ -160,6 +161,22 @@ int runSimulator5 (const FractureArgs& args, const char* contx)
     typedef SIMFracture<ElSolver,PhaseSolver,Cpl> Coupler;
     return runCombined<ElSolver,PhaseSolver,Coupler,Solver>(args.inpfile,contx);
   }
+}
+
+
+/*!
+  \brief Determines whether the explicit phase-field driver is to be used.
+*/
+
+template<class Dim, class ElSolver,
+         template<class T1, class T2> class Cpl,
+         template<class T1> class Solver=SIMSolver>
+int runSimulator5 (const FractureArgs& args, const char* context)
+{
+  if (args.expPhase)
+    return runSimulator6<ElSolver,SIMExplPhaseField,Cpl,Solver>(args,context);
+  else
+    return runSimulator6<ElSolver,SIMPhaseField<Dim>,Cpl,Solver>(args,context);
 }
 
 
@@ -241,14 +258,13 @@ template<class Dim, class Integrator, class ElSolver,
 int runSimulator3 (const FractureArgs& args)
 {
   typedef SIMDynElasticity<Dim,Integrator,ElSolver> DynElSolver;
-  typedef SIMPhaseField<Dim>                        PhaseSolver;
 
   const char* context = Integrator::inputContext;
 
   if (args.adap)
-    return runSimulator5<DynElSolver,PhaseSolver,Cpl,SIMSolverTS>(args,context);
+    return runSimulator5<Dim,DynElSolver,Cpl,SIMSolverTS>(args,context);
 
-  return runSimulator5<DynElSolver,PhaseSolver,Cpl>(args,context);
+  return runSimulator5<Dim,DynElSolver,Cpl>(args,context);
 }
 
 
@@ -363,10 +379,11 @@ int main (int argc, char** argv)
   if (!args.inpfile)
   {
     std::cout <<"usage: "<< argv[0]
-              <<" <inputfile> [-dense|-spr|-superlu[<nt>]|-samg|-petsc]\n      "
-              <<" [-lag|-spec|-LR] [-2D] [-mixed] [-nGauss <n>]\n       [-nocra"
-              <<"ck|-semiimplicit] [-[l|q]static|-GA|-HHT] [-poro] [-adaptive]"
-              <<"\n       [-vtf <format> [-nviz <nviz>] [-nu <nu>] [-nv <nv]"
+              <<" <inputfile> [-dense|-spr|-superlu[<nt>]|-samg|-petsc]\n"
+              <<"       [-lag|-spec|-LR] [-2D] [-mixed] [-nGauss <n>]\n"
+              <<"       [-nocrack|-explcrack|-semiimplicit]"
+              <<" [-[l|q]static|-GA|-HHT] [-poro] [-adaptive]\n"
+              <<"       [-vtf <format> [-nviz <nviz>] [-nu <nu>] [-nv <nv]"
               <<" [-nw <nw>]]\n       [-hdf5] [-principal]\n";
     return 0;
   }